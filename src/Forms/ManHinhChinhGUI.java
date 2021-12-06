3a223b1c011dfd445ea7250d945324dfc38d8dd4
thangvddz <thangvdph13678@fpt.edu.vn>
2021-12-06 21:11:06 +0700

    Cuongpull

--- src/Forms/ManHinhChinhGUI.java
+++ src/Forms/ManHinhChinhGUI.java
@@ -439,6 +439,9 @@ public class ManHinhChinhGUI extends javax.swing.JFrame {
             public void mouseExited(java.awt.event.MouseEvent evt) {
                 btnThanhToanMouseExited(evt);
             }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                btnThanhToanMousePressed(evt);
+            }
         });
 
         jLabel13.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -680,6 +683,11 @@ public class ManHinhChinhGUI extends javax.swing.JFrame {
         // TODO add your handling code here:
         resetColor(btnThanhToan);
     }//GEN-LAST:event_btnThanhToanMouseExited
+
+    private void btnThanhToanMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnThanhToanMousePressed
+        JPanel loaithanhtoan = new LoaiThanhToanJPanel();
+        SwitchPanel(loaithanhtoan);
+    }//GEN-LAST:event_btnThanhToanMousePressed
 
     public void init() {
         phongDAO = new PhongDAO();
